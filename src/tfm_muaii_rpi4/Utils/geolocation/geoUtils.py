d98b1ab4141b02afd5822387f876613970697e59
DavidEscri <davidescribano99@gmail.com>
2024-03-07 20:12:51 +0100

    Creación clase para definir coordenadas. Añadida clase de utiles para geolocalización y creado método a utilizar por el dispositivo GPS para saber si se está superando la velocidad máxima de la carretera.

--- src/tfm_muaii_rpi4/Utils/geolocation/geoUtils.py
+++ src/tfm_muaii_rpi4/Utils/geolocation/geoUtils.py
@@ -1,55 +1,154 @@
-from geopy.geocoders import Nominatim, OpenCage
-import osmnx as osm
-import requests
+import time
+import datetime
+
+from geopy.geocoders import Nominatim
+import geopy.distance
 
 
-def get_speed_limit(lat, lon):
-    geolocator = Nominatim(user_agent="my_geocoder")
-    location = geolocator.reverse((lat, lon), language='es')
+class Coordinates:
+    def __init__(self, latitude: float, longitude: float):
+        self.data: dict = {
+            "coordinates": (latitude, longitude),
+            "timestamp": time.time()
+        }
 
-    if location:
-        road_type = location.raw["type"]
-        place_id = location.raw["place_id"]
-        osm_id = location.raw["osm_id"]
-        road_data = osm.geocode_to_gdf(str(place_id))
-        max_speed = estimate_speed_limit(road_type)
-        if "address" in location.raw:
-            road_adress = location.raw["address"]
-            road_name = road_adress["road"]
-            #barrio = road_adress["suburb"]
-            ciudad = road_adress["city"]
-            provincia = road_adress["state_district"]
-            #comunidad = road_adress["state"]
-            pais = road_adress["country"]
-            print(f"La velocidad máxima para {road_name} ubicado en {ciudad} provincia de {provincia} ({pais}) es: "
-                  f"{max_speed} km/h")
+    def get_coordinates(self) -> tuple:
+        return self.data["coordinates"]
+
+    def get_timestamp(self) -> datetime:
+        return self.data["timestamp"]
+
+
+class GeoUtils:
+
+    @staticmethod
+    def get_location(coordenadas: Coordinates) -> geopy.location.Location:
+        """
+        Obtención de la localización definida por la libreria geopy a partir de unas coordenadas.
+        :param coordenadas: Coordenadas de las que se quiere conocer información
+        :return: Localización definida por la libreria de goepy
+        """
+        geolocator = Nominatim(user_agent="my_geocoder")
+        location = geolocator.reverse(coordenadas.get_coordinates(), language='es')
+        return location
+
+    def get_max_speed_location(self, location: geopy.location.Location) -> int:
+        """
+        Obtención de la máxima velocidad en km/h de una localizacicón definida por la libreria de geopy
+        :param location: Localización definida por la libreria de goepy
+        :return: Velocidad en km/h
+        """
+        max_speed: int = 0
+        if location:
+            road_type = self.get_road_type(location)
+            # place_id = location.raw["place_id"]
+            # osm_id = location.raw["osm_id"]
+            # road_data = osm.geocode_to_gdf(str(place_id))
+            max_speed = self.get_road_speed_limit(road_type)
+            if "address" in location.raw:
+                road_adress = location.raw["address"]
+                road_name = road_adress["road"]
+                # barrio = road_adress["suburb"]
+                ciudad = road_adress["city"] if "city" in road_adress else road_adress["town"]
+                provincia = road_adress["state_district"]
+                # comunidad = road_adress["state"]
+                # pais = road_adress["country"]
+                print(f"La velocidad máxima para {road_name} ubicado en {ciudad} ({provincia}) es: {max_speed} km/h")
         return max_speed
-    else:
-        print("Unable to reverse geocode coordinates.")
 
-    return None
+    @staticmethod
+    def get_road_type(location: geopy.location.Location) -> str:
+        """
+        Obtención del tipo de carretera
+        :param location: Localizacíon obtenida con la libreria de geopy
+        :return: Tipo de carretera definido por Nominatim
+        """
+        road_type: str = ""
+        if location:
+            road_type = location.raw["type"]
+        return road_type
+
+    @staticmethod
+    def get_road_speed_limit(road_type: str) -> int:
+        """
+        Obtención de la velocidad máxima a partir del tipo de carretera
+        :param road_type: Tipo de carretera definido por Nominatim.
+        :return: Velocidad máxima
+        """
+        speed_limits = {
+            "motorway": 120,  # Autovía / Autopista
+            "trunk": 100,  # Carretera convencional de doble calzada
+            "primary": 90,  # Carretera convencional de un solo carril
+            "secondary": 80,  # Carretera secundaria
+            "tertiary": 70,  # Carretera terciaria
+            "unclassified": 50,  # Carretera no clasificada
+            "residential": 30,  # Zona residencial
+            "service": 20,  # Vía de servicio
+        }
+        # Devolver la velocidad máxima según el tipo de carretera
+        return speed_limits.get(road_type, 0)
+
+    def calculate_speed(self, last_coordinates: Coordinates, current_coordinates: Coordinates) -> int:
+        """
+        Se calcula la velocidad en km/h a la que se está circulando a partir de dos coordenadas.
+        :param last_coordinates: Últimas coordenadas obtenidas.
+        :param current_coordinates: Coordenadas actuales.
+        :return: Velocidad actual
+        """
+        distance = geopy.distance.geodesic(last_coordinates.get_coordinates(),
+                                           current_coordinates.get_coordinates()).m
+        time_difference = current_coordinates.get_timestamp() - last_coordinates.get_timestamp()
+        if time_difference == 0:
+            return 0
+        speed = distance / time_difference
+        return self.convert_ms_to_kmh(speed)
+
+    @staticmethod
+    def convert_ms_to_kmh(speed: float) -> int:
+        """
+        Conversión de velocidad de m/s a km/h
+        :param speed: Velocidad en metros por segundo
+        :return: Velocidad en kilometros por hora
+        """
+        return round(speed * 3.6)
+
+    def is_speed_limit_exceeded(self, last_coordinates: Coordinates, current_coordinates: Coordinates) -> bool:
+        """
+        Se calcula la velocidad a la que se está circulando, y la compara con la velocidad máxima permitida para la
+        localización actual.
+        :param last_coordinates: Últimas coordenadas obtenidas.
+        :param current_coordinates: Coordenadas actuales.
+        :return: Booleano si la velocidad actual es superior a la máxima permitida
+        """
+        location = self.get_location(current_coordinates)
+        max_speed = self.get_max_speed_location(location)
+        current_speed = self.calculate_speed(last_coordinates, current_coordinates)
+        print(f"Velocidad actual: {current_speed} km/h")
+        return current_speed > max_speed
 
 
-def estimate_speed_limit(road_type: str):
-    # Definir velocidades máximas según el tipo de carretera en España
-    speed_limits = {
-        'motorway': 120,  # Autovía/Autopista
-        'trunk': 100,  # Carretera convencional de doble calzada
-        'primary': 90,  # Carretera convencional de un solo carril
-        'secondary': 70,  # Carretera convencional de un solo carril
-        'residential': 30  # Zona residencial
-    }
-    # Devolver la velocidad máxima según el tipo de carretera
-    return speed_limits.get(road_type, None)
+if __name__ == "__main__":
+    # Autopista del Mediterráneo, Daya Nueva, La Vega Baja del Segura, Alicante, Comunidad Valenciana, 03106, España
+    #latitude = 38.1194
+    #longitude = -0.7785
 
-def calculate_speed(last_coordinates, current_coordinates):
-    pass
+    # coordenadas: Coordinates = Coordinates(38.28931, -0.70968)
+    # speed_limit = get_coordinates_data(coordenadas)
+    geo_utils = GeoUtils()
+    coords1 = Coordinates(38.11219, -0.78666)
+    time.sleep(1)  # Simular el paso de un segundo
+    coords2 = Coordinates(38.11254, -0.78696)
+    if geo_utils.is_speed_limit_exceeded(coords1, coords2):
+        print("Velocidad límite superada")
+    # speed = calculate_speed(coords1, coords2)
+    # speed_limit_2 = get_coordinates_data(coords1)
+    # print(speed)
 
-# Autopista del Mediterráneo, Daya Nueva, La Vega Baja del Segura, Alicante, Comunidad Valenciana, 03106, España
-#latitude = 38.1194
-#longitude = -0.7785
-
-#
-latitude = 38.28931
-longitude = -0.70968
-speed_limit = get_speed_limit(latitude, longitude)
+    coords1 = Coordinates(38.11792, -0.77826)
+    time.sleep(1)
+    coords2 = Coordinates(38.11828, -0.77834)
+    if geo_utils.is_speed_limit_exceeded(coords1, coords2):
+        print("Velocidad límite superada")
+    # speed = calculate_speed(coords1, coords2)
+    # speed_limit_3 = get_coordinates_data(coords1)
+    # print(speed)
