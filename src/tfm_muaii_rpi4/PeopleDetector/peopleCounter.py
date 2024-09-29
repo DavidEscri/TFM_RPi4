30cc1469b173a14e918e18b35285c1ffb8f2bda0
DavidEscri <davidescribano99@gmail.com>
2024-09-29 19:51:59 +0200

    Optimización general para la detección de personas. Las predicciones ahora directamente filtran para solamente considerar personas. Corrección en el dibujado del bounding box de la imagen.

--- src/tfm_muaii_rpi4/PeopleDetector/peopleCounter.py
+++ src/tfm_muaii_rpi4/PeopleDetector/peopleCounter.py
@@ -21,10 +21,31 @@ from tfm_muaii_rpi4.Utils.utils import Service
 Logs = LogsSingleton()
 
 
-class _PeopleCounter(Service):
+class ImageDetectionConfig:
+    # Tamaño para capturar la imagen para que se utilice todo el sensor óptico
+    ORIGINAL_WIDTH = 1640
+    ORIGINAL_HEIGHT = 1232
+    ORIGINAL_SIZE = (ORIGINAL_WIDTH, ORIGINAL_HEIGHT)
+
+    # Tamaño adaptado al utilizado en el set de entrenamiento de YOLOv5 para mejorar el resultadod e las predicciones
+    NEW_WIDTH = 640
+    NEW_HEIGHT = 640
+    NEW_SIZE = (NEW_WIDTH, NEW_HEIGHT)
+
+    # Formato de imagen
+    FORMAT = "RGB888"
+
+    # Límite inferior de confianza en las detecciones
     CONF_THRESHOLD: float = 0.1
+
+    # Límite inferior para eliminar detecciones redundantes
     IOU_THRESHOLD: float = 0.5
+
+    # Factor de filtro
     FILTER_FACTOR: float = 0.8
+
+
+class _PeopleCounter(Service):
 
     def __init__(self, show_image=False):
         super().__init__(__info__, is_thread=True)
@@ -36,25 +57,30 @@ class _PeopleCounter(Service):
         self.__classes: list = []
         self.__init_camara()
 
-        self.sleep_period = 1
+        self.sleep_period = 0.5
 
     def __init_camara(self):
         self.__device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model_path = self._env.get_path(self._env.yolo_models_path)
         model_file = os.path.join(model_path, "yolov5n.pt")
-        self.__model = attempt_load(model_file, device=self.__device)
-        self.__model.to(self.__device).eval()
+        self.__model = attempt_load(model_file, device=self.__device).eval()
 
-        class_path = self._env.get_path(self._env.yolo_classes_path)
-        class_file = os.path.join(class_path, "coco.yaml")
-        self._load_yaml(class_file)
+        if self.__show_image:
+            self.__load_yaml()
 
         self.__camera = Picamera2()
-        camera_config = self.__camera.create_preview_configuration(main={"size": (640, 480), "format": "RGB888" })
+        camera_config = self.__camera.create_preview_configuration(
+            main={
+                "size": ImageDetectionConfig.ORIGINAL_SIZE,
+                "format": ImageDetectionConfig.FORMAT
+            }
+        )
         self.__camera.configure(camera_config)
 
-    def _load_yaml(self, yaml_file):
-        with open(yaml_file, "r", encoding="utf-8") as f:
+    def __load_yaml(self):
+        class_path = self._env.get_path(self._env.yolo_classes_path)
+        class_file = os.path.join(class_path, "coco.yaml")
+        with open(class_file, "r", encoding="utf-8") as f:
             data = yaml.safe_load(f)
             self.__classes = data["names"]
 
@@ -68,6 +94,8 @@ class _PeopleCounter(Service):
     def stop(self):
         try:
             self.__camera.stop()
+            if self.__show_image:
+                cv2.destroyAllWindows()
             super().stop()
         except Exception as e:
             super().critical_error(e, "stop")
@@ -82,35 +110,17 @@ class _PeopleCounter(Service):
                 if frame is None:
                     Logs.get_logger().error("Error al leer el fotograma de la camara", extra=__info__)
                 else:
+                    frame = cv2.flip(frame, 0)
                     self.__process_frame(frame)
+                    if self.__show_image:
+                        cv2.namedWindow("Picamera2 - Object Detection", cv2.WINDOW_NORMAL)
+                        cv2.imshow("Picamera 2 - Object Detection", frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            self.stop()
                 super().sleep_period()
         except Exception as e:
             Logs.get_logger().error(f"Error en el run del detector de personas: {e}", extra=__info__)
             super().critical_error(e, "_run")
-
-    # def _run_cv2(self):
-    #     try:
-    #         cap = cv2.VideoCapture(0)
-    #
-    #         if not cap.isOpened():
-    #             Logs.get_logger().critical("No se pudo abrir la cámara para obtener imagenes.", extra=__info__)
-    #             raise Exception("No se pudo abrir la cámara para obtener imagenes.")
-    #
-    #         while not super().need_stop():
-    #             ret, frame = cap.read()
-    #             if not ret:
-    #                 Logs.get_logger().error("Error al leer el fotograma de la camara", extra=__info__)
-    #             else:
-    #                 self.__process_frame(frame)
-    #                 Logs.get_logger().info(f"Recuento de personas: {round(self.__smoothed_person_count)}",
-    #                                        extra=__info__)
-    #             super().sleep_period()
-    #
-    #         cap.release()
-    #         cv2.destroyAllWindows()
-    #     except Exception as e:
-    #         Logs.get_logger().error(f"Error en el run del detector de personas: {e}", extra=__info__)
-    #         super().critical_error(e, "_run")
 
     # def _run_test_images(self):
     #     try:
@@ -134,65 +144,48 @@ class _PeopleCounter(Service):
     #         super().critical_error(e, "_run")
 
     def __process_frame(self, frame):
-        pred = self.__detect(frame)
-        if pred is not None and len(pred) > 0:
-            # det = pred  # Todas las detecciones
-            # det[:, :4] = xyxy2xywh(det[:, :4])  # Convertir coordenadas a xywh
-
-            #xywh = det[:, :4].tolist()
-            labels_detected = pred[:, 5].int().tolist()
-            # confidences = pred[:, 4].tolist()
-
-            # for label, confidence, bbox in zip(labels, confidences, xywh):
-            #     object_name = self.__classes[label]
-            #     # print(f"Object detected: {object_name}, Confidence: {confidence:.2f}, Bounding Box: {bbox}")
-            #     self._draw_bounding_box(frame, bbox, object_name, confidence)
-
-            self.__count_people(labels_detected)
-
-            # Mostrar el frame con las detecciones
-            # if self.__show_image:
-            #     cv2.imshow("YOLOv5 Object Detection", frame)
-            #
-            #     # Salir al presionar 'q'
-            #     if cv2.waitKey(1) & 0xFF == ord('q'):
-            #         self.stop()
+        prediction = self.__detect(frame)
+        if prediction is not None and len(prediction) > 0:
+            person_count = len(prediction)
+            if self.__show_image:
+                for detection in prediction:
+                    prediction[:, :4] = xyxy2xywh(prediction[:, :4])
+                    x, y, w, h = detection[:4]
+                    confidence = detection[4].item()  # Obtenemos la confianza de la prediccion
+                    class_id = int(detection[5].item())
+                    label = self.__classes[class_id]
+                    self._draw_bounding_box(frame, (x, y, w, h), label, confidence)
+            self.__update_people_count(person_count)
 
     def __detect(self, frame):
         img = torch.from_numpy(frame).to(self.__device)
         img = img.float() / 255.0
         img = img.permute(2, 0, 1).unsqueeze(0)  # Cambiar el orden de los canales y agregar dimensión batch
+        with torch.no_grad():
+            img = torch.nn.functional.interpolate(img, size=ImageDetectionConfig.NEW_SIZE, mode='bilinear', align_corners=False)
+            pred = self.__model(img)[0]
+            return non_max_suppression(pred, ImageDetectionConfig.CONF_THRESHOLD, ImageDetectionConfig.IOU_THRESHOLD, classes=[0])[0]
 
-        pred = self.__model(img)[0]
-        pred = non_max_suppression(pred, self.CONF_THRESHOLD, self.IOU_THRESHOLD)[0]
+    @staticmethod
+    def _draw_bounding_box(frame, bbox, label, confidence):
+        x, y, w, h = bbox
+        scale_x = ImageDetectionConfig.ORIGINAL_WIDTH / ImageDetectionConfig.NEW_WIDTH
+        scale_y = ImageDetectionConfig.ORIGINAL_HEIGHT / ImageDetectionConfig.NEW_HEIGHT
+        x1, y1, x2, y2 = int((x - w / 2) * scale_x), int((y - h / 2) * scale_y), int((x + w / 2) * scale_x), int((y + h / 2) * scale_y)
 
-        return pred
-
-    # @staticmethod
-    # def _draw_bounding_box(frame, bbox, label, confidence):
-    #     if label == "person":
-    #         x, y, w, h = bbox
-    #         x1, y1, x2, y2 = int(x - w / 2), int(y - h / 2), int(x + w / 2), int(y + h / 2)
-    #         Logs.get_logger().debug(f"El frame es del tipo: {frame.dtype}", extra=__info__)
-    #
-    #         # Dibujar el bounding box en la imagen
-    #         cv2.rectangle(frame, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
-    #         cv2.putText(frame, f"{label}: {confidence:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    def __count_people(self, labels):
-        person_count = sum(1 for label in labels if self.__classes[label].lower() == 'person')
-        self.__update_people_count(person_count)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
+        cv2.putText(frame, f"{label}: {confidence:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     def __update_people_count(self, person_count):
         Logs.get_logger().debug(f"Nueva detección de personas {self.get_current_people()} -> {person_count}",
                                 extra=__info__)
         if self.get_current_people() == 0:  # Se le da mayor importancia a las nuevas personas detectadas
             self.__smoothed_person_count = (
-                (1 - self.FILTER_FACTOR) * self.__smoothed_person_count + self.FILTER_FACTOR * person_count
+                (1 - ImageDetectionConfig.FILTER_FACTOR) * self.__smoothed_person_count + ImageDetectionConfig.FILTER_FACTOR * person_count
             )
         else: # Se le da mayor importancia a las personas anteriormente detectadas
             self.__smoothed_person_count = (
-                    self.FILTER_FACTOR * self.__smoothed_person_count + (1 - self.FILTER_FACTOR) * person_count
+                    ImageDetectionConfig.FILTER_FACTOR * self.__smoothed_person_count + (1 - ImageDetectionConfig.FILTER_FACTOR) * person_count
             )
         self.__set_current_people()
 
